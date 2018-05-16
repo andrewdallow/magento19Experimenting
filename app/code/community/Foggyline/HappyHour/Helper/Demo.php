5c21feb661ab2b6eb6b683f02f2081acfdc37dc7
Andew Dallow <andrew.dallow@ecommistry.com>
2018-05-16 16:02:45 +1200

    Added System Config Options and Helper.

---
+++ app/code/community/Foggyline/HappyHour/Helper/Demo.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Helper Class
+ *
+ *
+ * @category   Zend
+ * @package    Zend_Foggyline
+ * @subpackage HappyHour
+ * @copyright  Copyright (c) 2018 ecommistry (http://www.ecommistry.com)
+ * @license    http://framework.zend.com/license   BSD License
+ * @version    Release: @package_version@
+ * @link       http://framework.zend.com/package/PackageName
+ */
+
+class Foggyline_HappyHour_Helper_Demo
+{
+    public function help()
+    {
+        echo 'Don\'t ask me';
+    }
+}
